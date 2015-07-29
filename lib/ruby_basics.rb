4dca45b26f256c6d24c663899d8c926979d9b912
Tyler Machen <tylermachen@gmail.com>
2015-07-29 15:10:24 -0400

    complete ruby basics quiz

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,29 @@
 def division(num1, num2)
- 
+  num1 / num2
 end
 
 def assign_variable(value)
-  
+  name = value
 end
 
-def argue
-  
+def argue(arg)
+  arg
 end
 
-def greeting
-
+def greeting(name, phrase)
+  name + " says " + phrase
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
-
+  "this phrase will not be returned"
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(arg1 = "cheese", arg2 = "pepperoni")
+  arg2
+  arg1
 end
